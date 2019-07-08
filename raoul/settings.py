bc8070884d91d8349450493fbc55b1608098f137
nitish <nitishxp@gmail.com>
2019-07-08 13:55:57 +0530

    fix

--- raoul/settings.py
+++ raoul/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# Extra places for collectstatic to find static files.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+WHITENOISE_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'root') 
 
 REST_FRAMEWORK = {'NON_FIELD_ERRORS_KEY': 'detail'}
